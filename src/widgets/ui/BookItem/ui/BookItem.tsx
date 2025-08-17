50c0c212a661b48373a7db7ddf056accfb575a4c
sergeji <kirr7619@gmail.com>
2025-08-17 16:34:21 +0200

    step 4.7

--- src/widgets/ui/BookItem/ui/BookItem.tsx
+++ src/widgets/ui/BookItem/ui/BookItem.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { useAuth } from "@/App/store/hooks/useAuth";
+import { GenreName } from "@/App/store/reducers/adminReducer/adminSchema";
 import { UserRole } from "@/App/store/reducers/authReducer/authSchema";
 import { getItemBookSelector } from "@/App/store/reducers/bookItemReducer/selectors";
 import { deleteItemBook } from "@/App/store/reducers/bookItemReducer/services/deleteItemBook";
@@ -40,7 +41,7 @@ export const BookItem = () => {
     const isLast = index === book?.genre?.length - 1 ? "." : ", ";
     return (
       <span key={el.id}>
-        {el.name.toUpperCase()}
+        {GenreName[el.name]}
         {isLast}
       </span>
     );
